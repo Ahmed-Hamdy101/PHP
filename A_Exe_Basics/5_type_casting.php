d48c106d046b00dca49ff2745c2ac18963ff22d4
Ahmed Hamdy <94504141+Ahmed-Hamdy101@users.noreply.github.com>
2023-09-16 20:53:16 +0000

    add typecasting

---
+++ A_Exe_Basics/5_type_casting.php
@@ -0,0 +1,17 @@
+<?php
+
+// typecasting
+$int = 3;
+$float = (float) $int;
+print($float);
+
+$string = "3";
+$char = $string[0];
+print($char);
+
+
+$toInterger = (int) $string;
+$toFloat = (float) $string;
+
+
+?>
